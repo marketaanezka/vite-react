6be0c9dc43e9c59bf6e4509835d1728415245d78
Marketa Willis <marketa.willis@applifting.cz>
2024-05-31 11:33:50 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [name, setName] = useState("")
+  const [greet, setGreet] = useState(false)
+
+  const handleChange = (e) => { setName(e.target.value) }
+
+  const handleGreet = () => { setGreet(true) }
+
+  return (
+    <>
+      {greet ? <h1>Hi, {name}!</h1> : <h1>Hi, who's there?</h1>}
+      <div className="card">
+        <input type="text" name="person" id="" value={name} onChange={handleChange} />
+      </div>
+      <div>
+        <button onClick={handleGreet}>Say hi</button>
+      </div>
+    </>
+  )
+}
+
+export default App
