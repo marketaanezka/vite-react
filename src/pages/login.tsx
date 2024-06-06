d21d9797c9e9584acc3b496e4532c5ae8a71c73c
Marketa Willis <marketa.willis@applifting.cz>
2024-06-06 14:37:11 +0200

    connect google to login page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,50 +1,47 @@
-import React, { useState } from 'react';
+import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+import Cookies from "js-cookie";
 
-const Login: React.FC = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+export default function Login() {
+  const navigate = useNavigate();
+  const [isLoggedin, setIsLoggedin] = useState(false);
 
-  const handleUsernameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setUsername(event.target.value);
+  const handleClick = () => {
+    const callbackUrl = `${window.location.origin}`;
+    const googleClientId = process.env.REACT_APP_GOOGLE_CLIENT_ID;
+    const targetUrl = `https://accounts.google.com/o/oauth2/auth?redirect_uri=
+    ${encodeURIComponent(callbackUrl)}
+    &response_type=token&client_id=${googleClientId}&scope=openid%20email%20profile`;
+    window.location.href = targetUrl;
   };
 
-  const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setPassword(event.target.value);
-  };
+  useEffect(() => {
+    const accessTokenRegex = /access_token=([^&]+)/;
+    const isMatch = window.location.href.match(accessTokenRegex);
 
-  const handleSubmit = (event: React.FormEvent) => {
-    event.preventDefault();
-    // Perform login logic here
-    console.log('Username:', username);
-    console.log('Password:', password);
-  };
+    if (isMatch) {
+      const accessToken = isMatch[1];
+      Cookies.set("access_token", accessToken);
+      setIsLoggedin(true);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (isLoggedin) {
+      navigate("/secure");
+    }
+  }, [isLoggedin, navigate]);
 
   return (
-    <div>
-      <h1>Login Page</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="username">Username:</label>
-          <input
-            type="text"
-            id="username"
-            value={username}
-            onChange={handleUsernameChange}
-          />
+    <div className="root">
+      <div>
+        <h1>Log in with Google</h1>
+        <div className="btn-container">
+          <button className="btn btn-primary" onClick={handleClick}>
+            Log in with Google
+          </button>
         </div>
-        <div>
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={handlePasswordChange}
-          />
-        </div>
-        <button type="submit">Login</button>
-      </form>
+      </div>
     </div>
   );
-};
-
-export default Login;
+}
