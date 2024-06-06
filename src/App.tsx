2b619b21b94ec67f912f642fd1120a3b7e50153e
Marketa Willis <marketa.willis@applifting.cz>
2024-06-06 14:12:04 +0200

    add basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,38 @@
-import { useState } from 'react'
-import './App.css'
+import React, { useState } from 'react';
+import './App.css';
 
 function App() {
-  const [name, setName] = useState("")
-  const [greet, setGreet] = useState(false)
+  const [name, setName] = useState("");
+  const [greet, setGreet] = useState(false);
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => { setName(e.target.value) }
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value);
+  };
 
-  const handleGreet = () => { setGreet(true) }
+  const handleGreet = () => {
+    setGreet(true);
+  };
 
   return (
-    <>
-      {greet ? <h1>Hi, {name}!</h1> : <h1>Hi, who's there?</h1>}
-      <div className="card">
-        <input type="text" name="person" id="" value={name} onChange={handleChange} />
-      </div>
+    <div>
+      <a href="/login">Login</a>
       <div>
-        <button onClick={handleGreet}>Say hi</button>
+        {greet ? <h1>Hi, {name}!</h1> : <h1>Hi, who's there?</h1>}
+        <div className="card">
+          <input
+            type="text"
+            name="person"
+            id=""
+            value={name}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <button onClick={handleGreet}>Say hi</button>
+        </div>
       </div>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
